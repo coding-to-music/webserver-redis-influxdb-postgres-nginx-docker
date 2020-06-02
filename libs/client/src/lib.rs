1e7d5b043e6c0e9bc538ad2d61418d8f5f541415
Isak Jägberg <ijagberg@gmail.com>
2020-06-02 20:39:03 +0200

    Improve client building

--- libs/client/src/lib.rs
+++ libs/client/src/lib.rs
@@ -1,17 +1,21 @@
+use std::fmt::Display;
 use webserver_contracts::{JsonRpcRequest, JsonRpcResponse};
 
 pub struct WebserverClient {
     url: String,
-    inner_client: reqwest::Client,
+    client: reqwest::Client,
 }
 
 impl WebserverClient {
-    fn new(url: String) -> Self {
+    pub fn new() -> WebserverClientBuilder {
+        WebserverClientBuilder::default()
+    }
+
+    fn from_builder(builder: WebserverClientBuilder) -> Result<Self, WebserverBuilderError> {
+        let url = builder.url.ok_or(WebserverBuilderError::MissingUrl)?;
         let client = reqwest::Client::new();
-        Self {
-            url,
-            inner_client: client,
-        }
+
+        Ok(Self { url, client })
     }
 
     pub async fn send_request(
@@ -19,7 +23,7 @@ impl WebserverClient {
         request: JsonRpcRequest,
     ) -> Result<JsonRpcResponse, WebserverClientError> {
         let response: JsonRpcResponse = self
-            .inner_client
+            .client
             .post(&self.url)
             .body(request.as_formatted_json())
             .send()
@@ -42,18 +46,44 @@ impl WebserverClientBuilder {
     }
 
     pub fn build(self) -> Result<WebserverClient, WebserverBuilderError> {
-        Ok(WebserverClient::new(
-            self.url.ok_or(WebserverBuilderError::MissingUrl)?,
-        ))
+        WebserverClient::from_builder(self)
     }
 }
 
+impl Default for WebserverClientBuilder {
+    fn default() -> Self {
+        Self { url: None }
+    }
+}
+
+#[derive(Debug)]
 pub enum WebserverBuilderError {
     MissingUrl,
 }
 
+impl Display for WebserverBuilderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let output = match self {
+            WebserverBuilderError::MissingUrl => "missing url",
+        };
+
+        write!(f, "{}", output)
+    }
+}
+
+#[derive(Debug)]
 pub enum WebserverClientError {
     ReqwestError(reqwest::Error),
+}
+
+impl Display for WebserverClientError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let output = match self {
+            WebserverClientError::ReqwestError(e) => format!("reqwest error: '{}'", e),
+        };
+
+        write!(f, "{}", output)
+    }
 }
 
 impl From<reqwest::Error> for WebserverClientError {
