27db76e3a329353611db9cdbdbe338b1f6ae24f9
Isak Jägberg <ijagberg@gmail.com>
2022-01-31 10:45:19 +0100

    -

--- src/redis/src/sync_pool.rs
+++ src/redis/src/sync_pool.rs
@@ -7,17 +7,18 @@ use r2d2_redis::{
 use std::{error::Error, time};
 
 pub struct SyncRedisPool {
+    addr: String,
     pool: r2d2_redis::r2d2::Pool<RedisConnectionManager>,
 }
 
 impl SyncRedisPool {
-    pub fn new(addr: &str, count: usize) -> Self {
-        let manager = RedisConnectionManager::new(addr).unwrap();
+    pub fn new(addr: String, count: u32) -> Self {
+        let manager = RedisConnectionManager::new(addr.clone()).unwrap();
         let pool = r2d2::Pool::builder()
             .max_size(count)
             .build(manager)
             .unwrap();
-        Self { pool }
+        Self { addr, pool }
     }
 
     pub fn get_connection(
